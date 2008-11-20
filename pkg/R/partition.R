fefecaa70f12f4dd32c033c619a4b3fd3debebdf
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-20 16:30:32 +0000

    Added function treePart
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adephylo@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/partition.R
+++ pkg/R/partition.R
@@ -6,20 +6,52 @@
 ############
 # listTips
 ############
-listTips <- function(tree){
+listTips <- function(x){
     if(!require(phylobase)) stop("phylobase package is not installed")
 
-    x <- tree
-    ## conversion from phylo4 and phylo4d
+    ## conversion from phylo, phylo4 and phylo4d
     x <- as(x, "phylo4")
 
     ## check phylo4 object
-    if (is.character(checkval <- check_phylo4(res))) stop(checkval)
+    if (is.character(checkval <- check_phylo4(x))) stop(checkval)
 
     ## computations
     res <- lapply(nodeLabels(x), function(e) descendants(x, e))
 
     names(res) <- nodeLabels(x)
+
+    return(res)
+}
+
+
+
+
+
+###########
+# treePart
+###########
+treePart <- function(x){
+    if(!require(phylobase)) stop("phylobase package is not installed")
+
+    ## conversion from phylo, phylo4 and phylo4d
+    x <- as(x, "phylo4")
+
+    ## check phylo4 object
+    if (is.character(checkval <- check_phylo4(x))) stop(checkval)
+
+    n <- nTips(x)
+
+    ## function coding one dummy vector
+    fDum <- function(vec){ # vec is a vector of tip numbers
+        dum <- integer(n)
+        dum[vec] <- 1
+        return(dum)
+    }
+
+    ## main computations
+    temp <- listTips(x)
+    res <- data.frame(lapply(temp,fDum))
+    row.names(res) <- x@tip.label
 
     return(res)
 }
