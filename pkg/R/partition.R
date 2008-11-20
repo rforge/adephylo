0be6b3d819803ac706885234a398c8dab1750746
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-20 17:21:32 +0000

    Added listNodes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adephylo@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/partition.R
+++ pkg/R/partition.R
@@ -3,6 +3,10 @@
 ## For instance to obtain dummy vectors used in the orthogram.
 ##
 
+
+
+
+
 ############
 # listTips
 ############
@@ -16,12 +20,38 @@ listTips <- function(x){
     if (is.character(checkval <- check_phylo4(x))) stop(checkval)
 
     ## computations
-    res <- lapply(nodeLabels(x), function(e) descendants(x, e))
+    res <- lapply(1:nNodes(x), function(i) descendants(x, i))
 
-    names(res) <- nodeLabels(x)
+    if(hasNodeLabels(x)) {names(res) <- nodeLabels(x)}
 
     return(res)
-}
+} # end listTips
+
+
+
+
+
+############
+# listNodes
+############
+listNodes <- function(x){
+    if(!require(phylobase)) stop("phylobase package is not installed")
+
+    ## conversion from phylo, phylo4 and phylo4d
+    x <- as(x, "phylo4")
+
+    ## check phylo4 object
+    if (is.character(checkval <- check_phylo4(x))) stop(checkval)
+
+    ## computations
+    nodIdx <- nTips(x)+1
+    nodIdx <- nodIdx:(nodIdx+nNodes(x)-1)
+    res <- lapply(nodIdx, function(i) children(x, i))
+
+    if(hasNodeLabels(x)) {names(res) <- nodeLabels(x)}
+
+    return(res)
+} # end listNodes
 
 
 
@@ -52,6 +82,7 @@ treePart <- function(x){
     temp <- listTips(x)
     res <- data.frame(lapply(temp,fDum))
     row.names(res) <- x@tip.label
+    res <- res[,-1]
 
     return(res)
 }
