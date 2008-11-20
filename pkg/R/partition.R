837f08f388322c59b3bc25467ea2591b47995f0f
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-20 18:39:10 +0000

    Added a distRoot function.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adephylo@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/partition.R
+++ pkg/R/partition.R
@@ -119,7 +119,7 @@ path2tips <- function(x, tip1, tip2){
     res <- getnodes(res)
 
     return(res)
-}
+} # end path2tips
 
 
 
@@ -130,14 +130,24 @@ path2tips <- function(x, tip1, tip2){
 ###########
 distRoot <- function(x, tip){
     if(!require(phylobase)) stop("phylobase package is not installed")
-    
+
     ## conversion from phylo, phylo4 and phylo4d
     x <- as(x, "phylo4")
-    
+
     ## check phylo4 object
     if (is.character(checkval <- check_phylo4(x))) stop(checkval)
-    
+
     ## main computations
-  tip <- getnodes(tip)
-    
-}
+    tip <- getnodes(x, tip)
+    root <- getnodes(x, nTips(x)+1)
+    ancTip <- ancestors(x, tip, which="all")
+
+    pathNodes <- setdiff(ancTip, root) # only internal nodes, without root
+    pathNodes <- c(tip, pathNodes)
+
+    pathNodes <- getnodes(x, pathNodes)
+
+    res <- sumEdgeLength(x, pathNodes)
+
+    return(res)
+} # end distRoot
