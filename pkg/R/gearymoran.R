5c119fcc7e77bde70a924b190c30985e5d2d17f9
jombart <jombart@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-03 14:36:20 +0000

    Making some changes to gearymoran (passing it as a generic+ phylo4d method). Wait for agreement.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/adephylo@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/gearymoran.R
+++ pkg/R/gearymoran.R
@@ -1,28 +1,33 @@
-"gearymoran" <- function (bilis, X, nrepet=999,alter=c("greater", "less", "two-sided")) {
+gearymoran <- function (x, W, nrepet=999,alter=c("greater", "less", "two-sided")) {
+
+    ## some checks
+    if(!require(ade4)) stop("The ade4 package is not installed.")
     alter <- match.arg(alter)
-    ## bilis doit être une matrice
-    bilis <- as.matrix(bilis)
-    nobs <- ncol(bilis)
-    # bilis doit être carrée
-    if (nrow(bilis) != nobs) stop ("'bilis' is not squared")
-    # bilis doit être symétrique
-    bilis <- (bilis + t(bilis))/2
-    # bilis doit être à termes positifs (voisinages)
-    if (any(bilis<0)) stop ("term <0 found in 'bilis'")
-    test.names <- names(X)
-    X <- data.matrix(X)
-    if (nrow(X) != nobs) stop ("non convenient dimension")
-    nvar <- ncol(X)
+
+    ## checks for W
+    if (any(W<0)) stop ("term <0 found in 'W'")
+    if (nrow(W) != nobs) stop ("'W' is not squared")
+    W <- as.matrix(W)
+    nobs <- ncol(W)
+
+    ## W has to be symmetric
+    W <- (W + t(W))/2
+
+    ## main computations
+    test.names <- names(x)
+    x <- data.matrix(x)
+    if (nrow(x) != nobs) stop ("non convenient dimension")
+    nvar <- ncol(x)
     res <- .C("gearymoran",
         param = as.integer(c(nobs,nvar,nrepet)),
-        data = as.double(X),
-        bilis = as.double(bilis),
+        data = as.double(x),
+        W = as.double(W),
         obs = double(nvar),
         result = double (nrepet*nvar),
         obstot = double(1),
         restot = double (nrepet),
-        PACKAGE="ade4"
+        PACKAGE="adephylo"
     )
     res <- as.krandtest(obs=res$obs,sim=matrix(res$result,ncol=nvar, byr=TRUE),names=test.names,alter=alter)
     return(res)
-}
+} # end gearymoran
